d20a6939db4ef03a99a53d9532872e667f86539e
SravaniKusuma <88419869+SravaniKusuma@users.noreply.github.com>
2021-10-29 10:50:21 +0530

    story-book

---
+++ StoryBookPractice/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+  return (
+    <div className="App">
+     <Button variant='secondary'>Secondary</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ StoryBookPractice/src/components/Subscriptions/Subscriptions.stories.js
@@ -0,0 +1,14 @@
+import { Primary } from "../Button/Button.stories";
+import { Medium } from "../Input/Input.stories";
+
+export default{
+
+    title : 'form/Subscription'
+}
+
+export const PrimarySubscription = () => (
+    <>
+        <Medium />
+        <Primary />
+    </>
+)
